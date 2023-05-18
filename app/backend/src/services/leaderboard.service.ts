b4d4c4cf04b9090e1d6dd4512af357a80d744da5
Renan-Storrer <storrerrenan@gmail.com>
2023-05-18 15:14:40 -0300

    feat: implementa rotas leaderboards e suas verificações

---
+++ app/backend/src/services/leaderboard.service.ts
@@ -0,0 +1,61 @@
+import { ModelStatic } from 'sequelize';
+import { grResponse } from '../utils/grResponse';
+import { rank, grResults, orderRank, grResultsGeneral, rankGeneral } from '../utils/leaderboardFunctions';
+import IResponse from '../interfaces/IResponse';
+import Matche from '../database/models/MatcheModel';
+import Team from '../database/models/TeamModel';
+import ILeaderboard from '../interfaces/ILeaderboard';
+
+class LeaderboardService {
+  private _matche: ModelStatic<Matche> = Matche;
+  private _team: ModelStatic<Team> = Team;
+
+  async rankHome(): Promise<IResponse> {
+    const teams = await this._team.findAll();
+    const matches = await this._matche.findAll({ where: { inProgress: false } });
+
+    const result: ILeaderboard[] = [];
+
+    teams.forEach((e) => {
+      const matchesByTeam = matches.filter((el) => el.homeTeamId === e.id);
+      const results = grResults(matchesByTeam, ['homeTeamGoals', 'awayTeamGoals']);
+
+      result.push(rank(e, results, matchesByTeam, ['homeTeamGoals', 'awayTeamGoals']));
+    });
+    return grResponse(200, orderRank(result));
+  }
+
+  async rankAway(): Promise<IResponse> {
+    const teams = await this._team.findAll();
+    const matches = await this._matche.findAll({ where: { inProgress: false } });
+
+    const result: ILeaderboard[] = [];
+
+    teams.forEach((e) => {
+      const matchesByTeam = matches.filter((el) => el.awayTeamId === e.id);
+      const results = grResults(matchesByTeam, ['awayTeamGoals', 'homeTeamGoals']);
+
+      result.push(rank(e, results, matchesByTeam, ['awayTeamGoals', 'homeTeamGoals']));
+    });
+    return grResponse(200, orderRank(result));
+  }
+
+  async rankGeral(): Promise<IResponse> {
+    const teams = await this._team.findAll();
+    const matches = await this._matche.findAll({ where: { inProgress: false } });
+
+    const result: ILeaderboard[] = [];
+
+    teams.forEach((e) => {
+      const matchesByTeam = matches.filter(
+        (el) => el.awayTeamId === e.id || el.homeTeamId === e.id,
+      );
+      const results = grResultsGeneral(e.id, matchesByTeam);
+
+      result.push(rankGeneral(e, results, matchesByTeam));
+    });
+    return grResponse(200, orderRank(result));
+  }
+}
+
+export default LeaderboardService;
